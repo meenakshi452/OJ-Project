1c8b3d7985d0412a17a9306b7c1a1dc1b9ee41e8
meenakshi452 <bethim21@iitk.ac.in>
2024-08-05 07:39:32 +0530

    made  change in db connection

--- backend/database/db.js
+++ backend/database/db.js
@@ -4,8 +4,14 @@ dotenv.config();
 
 const DBConnection = async () => {
     const MONGODB_URL = process.env.MONGODB_URI;
+    const client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        serverSelectionTimeoutMS: 30000, // Increase to 30 seconds
+        socketTimeoutMS: 60000, // Increase to 60 seconds
+      });
     try {
-        await mongoose.connect(MONGODB_URL, {useNewUrlParser: true});
+        await client.connect(MONGODB_URL, {useNewUrlParser: true});
         console.log("DB connection established!!");
     } catch (error) {
         console.log(error);
